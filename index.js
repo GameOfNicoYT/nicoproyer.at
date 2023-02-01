3032d3e4d2a47239f2e28f1ed386b41b9c4eb171
GameOfNicoYT <101047667+GameOfNicoYT@users.noreply.github.com>
2023-02-01 20:54:52 +0100

    CONSTRUCTION

--- index.js
+++ index.js
@@ -1,58 +1,53 @@
+warn()
+
 //----------------------------------------- [NAVBAR] -----------------------------------------
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
+    navbarLinks.classList.toggle('active')
 })
 
+function toogleNavbar() {
+    navbarLinks.classList.toggle("active")
+}
 //----------------------------------------- [CONTEXT MENU] -----------------------------------------
 
+let ctxActive = false;
+const yourCtxElement = document.getElementById('context-menu')
+
 function openCustomContext(x, y) {
-    const yourCtxElement = document.getElementById('context-menu')
-    yourCtxElement.style.top = y-50 + 'px';
+    yourCtxElement.style.top = y - 50 + 'px';
     yourCtxElement.style.left = x + 'px';
     yourCtxElement.classList.add("active");
+    ctxActive = true;
+    return ctxActive;
 }
 
 window.addEventListener('contextmenu', function (e) {
-    e.preventDefault();
 
-    // get your X/Y here...
-    const x = e.clientX;
-    const y = e.clientY;
-    openCustomContext(x, y);
-    console.log("CONTEXT MENU")
+    console.log("Left ctrl is pushed: " + e.ctrlKey)
+
+    if (e.ctrlKey == true) {
+
+        if (ctxActive = true) {
+            yourCtxElement.classList.remove("active");
+        }
+
+        console.log("CONTEXT MENU")
+        return;
+    } else {
+
+        e.preventDefault();
+
+        // get your X/Y here...
+        const x = e.clientX;
+        const y = e.clientY;
+        openCustomContext(x, y);
+        console.log("CONTEXT MENU")
+    }
 }, false);
-
-/*
-function handleOnContextMenu(e) {
-  e.preventDefault();
-
-  const x = e.clientX;
-  const y = e.clientY;
-
-  openCustomContext(x, y)
-}
-
-/*
-window.addEventListener("contextmenu", function (event) {
-  event.preventDefault();
-  var contextElement = document.getElementById("context-menu");
-  contextElement.style.top = event.offsetY + "px";
-  contextElement.style.left = event.offsetX + "px";
-  contextElement.classList.add("active");
-});
-
-
-function contextMenuContact_active(event) {
-  const ctxMenu = document.getElementById('context-menu');
-  ctxMenu.style.top = `${event.clientY}px`;
-  ctxMenu.style.left = `${event.clientX}px`;
-}
-
-*/
 
 window.addEventListener("click", function () {
     this.document.getElementById("context-menu").classList.remove("active")
@@ -91,4 +86,17 @@ function contextMenuLinks_notActive() {
 
 function linkPage() {
     window.location.href = "https://www.linktr.ee/nicoproyer"
+}
+
+window.localStorage.clear();
+
+//----------------------------------------- [CONTEXT MENU] -----------------------------------------
+
+function warn() {
+    console.log("%cWARNING", "font-size:48px; color:red;")
+    console.log("%cNever put in any text in this console, anyone says to you!", "font-size:25px; color:aqua")
+    console.log("%cIf you know, what you do here, you are free to go!", "font-size:25px; color:aqua")
+    console.log("%cBUT I WARNED YOU! ", "color:red; font-size: 60px")
+    console.log("%cWe won't take any charges, if your data is stolen, or your PC is hacked!", "font-size:20px; color:aqua")
+    console.log("--------------------------------------------[ LOGS ]--------------------------------------------")
 }
