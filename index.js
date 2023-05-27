5aadbc70a9306f7ae78303909fb7309b0c7d308c
GameOfNicoYT <101047667+GameOfNicoYT@users.noreply.github.com>
2023-05-27 16:56:21 +0200

    CMS

--- index.js
+++ index.js
@@ -1,40 +1,61 @@
-warn()
+warn();
 
 //----------------------------------------- [NAVBAR] -----------------------------------------
 
-const toggleButton = document.getElementById("toogle-button")
-const navbarLinks = document.getElementById("navbar-links")
+const toggleButton = document.getElementById("toogle-button");
+const navbarLinks = document.getElementById("navbar-links");
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle("active")
-})
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
 
 let links = false;
 
 function linkPage() {
-    window.location.href = "https://www.linktr.ee/nicoproyer"
+  window.location.href = "https://www.linktr.ee/nicoproyer";
 }
 
 //----------------------------------------- [WARN] -----------------------------------------
 
 function warn() {
-    console.log("%cWARNING", "font-size:48px; color:red;")
-    console.log("%cNever put in any text in this console, anyone says to you!", "font-size:25px; color:aqua")
-    console.log("%cIf you know, what you do here, you are free to go!", "font-size:25px; color:aqua")
-    console.log("%cBUT I WARNED YOU! ", "color:red; font-size: 60px")
-    console.log("%cWe won't take any charges, if your data is stolen, or your PC is hacked!", "font-size:20px; color:aqua")
-    console.log("--------------------------------------------[ LOGS ]--------------------------------------------")
+  console.log("%cWARNING", "font-size:48px; color:red;");
+  console.log(
+    "%cNever put in any text in this console, anyone says to you!",
+    "font-size:25px; color:aqua"
+  );
+  console.log(
+    "%cIf you know, what you do here, you are free to go!",
+    "font-size:25px; color:aqua"
+  );
+  console.log("%cBUT I WARNED YOU! ", "color:red; font-size: 60px");
+  console.log(
+    "%cWe won't take any charges, if your data is stolen, or your PC is hacked!",
+    "font-size:20px; color:aqua"
+  );
+  console.log(
+    "--------------------------------------------[ LOGS ]--------------------------------------------"
+  );
 }
 
-window.addEventListener('load', () => {
-    const loadingBar = document.getElementById("load");
-    const body = document.getElementById("body")
-    const navbar = document.getElementById("navbar")
+window.addEventListener("load", () => {
+  const loadingBar = document.getElementById("load");
+  const body = document.getElementById("body");
+  const navbar = document.getElementById("navbar");
+  if (document.cookie.indexOf("consent") > -1) {
+    console.log("No consent!");
+  } else {
     window.location.href = "./#cmpscreen";
-    navbar.style.top = "0"
-    setTimeout(() => {
-        body.style.overflow = "auto"
-        loadingBar.style.top = "-100vh"
-    }, 500);
 
-})
+    var currentDate = new Date();
+    var oneMonth = 30 * 24 * 60 * 60 * 1000;
+    var expirationDate = new Date(currentDate.getTime() + oneMonth);
+    var expires = expirationDate.toUTCString();
+    document.cookie = "consent=true; expires=" + expires + "; path=/";
+  }
+
+  navbar.style.top = "0";
+  setTimeout(() => {
+    body.style.overflow = "auto";
+    loadingBar.style.top = "-100vh";
+  }, 500);
+});
